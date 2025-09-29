0f75b1dc9d934ef1506bff86df4bbd5df5a0b83f
rjeff-dev <jeff_xd00@hotmail.com>
2025-09-29 14:40:07 -0300

    footer

--- src/script.js
+++ src/script.js
@@ -1,22 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]')
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (event) {
             const abaAlvo = event.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
-
-            // esconde todas as abas e mostra só a clicada
             escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
-
-            // tira a classe ativa de todos os botões
             removeBotaoAtivo(buttons);
-            // coloca a classe ativa no botão clicado
-            event.target.classList.add('shows__tabs__button--is-active');
-            });
+                event.target.classList.add('shows__tabs__button--is-active');
+        });
+    }
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta);
     }
 });
+
+function abreOuFechaResposta(event){    
+    const classe = 'faq__question__item--is-open';
+    const item = event.currentTarget.closest('.faq__question__item');
+    item.classList.toggle(classe);
+    console.log(item); // agora loga o <li> que foi clicado
+}
+
 
 function removeBotaoAtivo(buttons) {
     for (let i = 0; i < buttons.length; i++) {
