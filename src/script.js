dcf275193639ccb53c44ecbcf91d1fe6e0c33770
rjeff-dev <jeff_xd00@hotmail.com>
2025-10-09 16:00:40 -0300

    modulo-21

--- src/script.js
+++ src/script.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const questions = document.querySelectorAll('[data-faq-question]')
+    const questions = document.querySelectorAll('[data-faq-question]');
+    
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
 
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+        console.log(posicaoAtual);
+
+        if(posicaoAtual < alturaHero){
+            ocultaElementosHeader();
+            console.log();
+        }else {
+            exibeElementosHeader();
+        }
+    })
+
+    // seção atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (event) {
             const abaAlvo = event.target.dataset.tabButton;
@@ -17,19 +33,30 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+function ocultaElementosHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+//faq e acordion
 function abreOuFechaResposta(event){    
     const classe = 'faq__question__item--is-open';
     const item = event.currentTarget.closest('.faq__question__item');
     item.classList.toggle(classe);
     console.log(item); // agora loga o <li> que foi clicado
-}
+};
 
 
 function removeBotaoAtivo(buttons) {
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
-}
+};
 
 function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
@@ -37,4 +64,4 @@ function escondeTodasAbas() {
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
-}
+};
